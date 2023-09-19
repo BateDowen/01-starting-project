a85134a287cb44f721ce73bec69f07faa0c1f44e
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-09-19 10:40:46 +0300

    initialize app with starting html and css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomePage from "./components/travel-agency-html-template/HomePage";
+
+
+function App() {
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
